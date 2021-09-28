0917d4a71a5905dfddede12d435f023d23c22d07
siddharth teli <telisiddharth123@gmail.com>
2021-09-28 23:48:29 +0530

    Error:then & catch

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,36 @@ class App extends React.Component<{}, { balance: number}> {
     
 
   }
+  clicked = async () => {
+    let bal=balance('FrkQkg5fn9v7KGN7b2nK4Q5Mub4AzUhZAeLPcmX6TQcC').then((balance) => {
+      return balance
+
+    }).catch((e)=>{
+      console.log("Error hai---"+e.toString())
+    })
+    //this.setState({balance:await balance('FrkQkg5fn9v7KGN7b2nK4Q5Mub4AzUhZAeLPcmX6TQcC')});
+    alert("Called")
+
+  }
   render() 
   {
+   
     
     return (
       <div className="App">
       <p>Value of Balance:--{this.state.balance}</p>
+      <form onSubmit={this.clicked}>
+        <input type="submit"value="Click"/>
+      </form>
       </div>
     );
   }
 
   async componentDidMount() {
-    this.setState({balance:await balance('FrkQkg5fn9v7KGN7b2nK4Q5Mub4AzUhZAeLPcmX6TQcC')});
+    //this.setState({balance:await balance('FrkQkg5fn9v7KGN7b2nK4Q5Mub4AzUhZAeLPcmX6TQcC')});
 
   }
+
 
   
   
