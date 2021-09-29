353eb57d85a02690c3fba8d5565d8bf6b43a141c
siddharth teli <telisiddharth123@gmail.com>
2021-09-29 17:04:56 +0530

    Add:Balance component

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { isThisTypeNode } from "typescript";
 import   {balance,temp} from '../../helpers/userBalance';
 
 
@@ -15,11 +16,14 @@ export default class Balance extends React.Component
       this.setState({balance: await balance()});
      alert("Balance "+this.state.balance);
       }
+      onChange=(e) => {
+        this.setState({address: e.target.value})
+      }
   render() {
         return <div>
           
             <label>Check Balance:</label>
-              
+               <input type="text" value={this.state.address} onChange={this.onChange} />
               <button onClick={this.onClick}>Check</button>
             
             <h1>Balance of {this.state.address} is {this.state.balance}</h1>
